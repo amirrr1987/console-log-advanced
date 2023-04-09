6caf93a1c0022cac4052f2db063759724ea11506
Amir Maghami <Maghami.A1987@gmail.com>
2023-04-09 14:28:53 +0430

    feat: product log

--- index.js
+++ index.js
@@ -2,29 +2,40 @@ class Logger {
 
   constructor({ isDevelopMode }) {
     this._isDevelopMode = isDevelopMode
+    this._count = 0
+
   }
   message({ name, value }) {
-    this._isDevelopMode ? this.#_runDevelop(name, value) : this.#_runProduct()
-
+    if (this._isDevelopMode) {
+      this.#_runDevelop(name, value)
+    }
+    else {
+      this._count += 1
+      if (this._count === 1) {
+        this.#_runProduct()
+      }
+    }
   }
   #_runDevelop(name, value) {
     console.log('                                                               ');
     console.log('%c--------------------------------------------------------------', 'color: red');
     console.log('%c--------------------- Start log ------------------------------', 'color: red');
+    console.log('                                                               ');
     console.time("log duration is:");
     const res = Object.prototype.toString.call(value)
     const dataType = res.split('[object')[1].split(']')[0]
     console.log(`Type is: ${dataType}`);
     console.log(name, value);
+    console.log('                                                               ');
     console.timeEnd("log duration is:");
     console.log('%c---------------------- End log ------------------------------', 'color: red');
     console.log('%c-------------------------------------------------------------', 'color: red');
     console.log('                                                               ');
   }
-
   #_runProduct() {
-    console.log('Sorry we are in production mode..')
+    console.log('%cSorry we are in production mode..', 'color: red')
   }
+
 }
 module.exports = Logger;
 
