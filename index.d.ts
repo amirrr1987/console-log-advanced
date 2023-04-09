0cbe61555f103db5830a1092adec8d70a91bce57
Amir Maghami <Maghami.A1987@gmail.com>
2023-04-09 14:33:25 +0430

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 declare class Logger {
   private _isDevelopMode: boolean;
+  private _count: number;
   constructor(options: { isDevelopMode: boolean });
   message(options: { name: string, value: any }): void;
   private #_runDevelop(name: string, value: any): void;
