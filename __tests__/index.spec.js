0165f65d9d42ec77abfadecc0fe4c32541001ed3
Amir Maghami <Maghami.A1987@gmail.com>
2023-04-11 16:57:41 +0430

    feat: deactive option

--- __tests__/index.spec.js
+++
@@ -1,8 +0,0 @@
-import Logger from '../index'
-
-const logger = new Logger({ isDevelopMode: true })
-describe("Calculator tests", () => {
-  test('adding 1 + 2 should return 3', () => {
-    expect(logger.message({ value: 88 }))
-  });
-})
