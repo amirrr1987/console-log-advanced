88fbc1ff1d0349eb6b122ab1ab4aee74ed64a668
Amir Maghami <Maghami.A1987@gmail.com>
2023-07-16 13:48:15 +0430

    fix: log

--- index.js
+++ index.js
@@ -1,71 +1,73 @@
-const _isDevelop = Symbol("isDevelop");
-const _count = Symbol("count");
+const _isDevelop = Symbol('isDevelop')
+const _count = Symbol('count')
 
 const colors = {
-  title: "color: red",
-  info: "color: blue",
-};
+  title: 'color: red',
+  info: 'color: blue',
+}
 
 const getDataType = (value) => {
-  return Object.prototype.toString.call(value).split("[object ")[1].split("]")[0];
-};
+  return Object.prototype.toString.call(value).split('[object ')[1].split(']')[0]
+}
 
-const logHeader = () => {
-  console.groupCollapsed("%c---------- Start log -----------", colors.title);
-};
+const logHeader = (title, collapsed) => {
+
+  collapsed ? console.groupCollapsed(`%c---------- Start ${title} -----------`, colors.title) : console.group(`%c---------- Start ${title} -----------`, colors.title)
+
+}
 
 const logFooter = () => {
-  console.groupEnd("%c----------- End log ------------", colors.title);
-};
+  console.groupEnd('%c----------- End log ------------', colors.title)
+}
 
 const logDetails = (label, content) => {
-  if (!content) return;
-  console.info(`%c${label}: %c${content}`, colors.info, colors.title);
-};
+  if (!content) return
+  console.info(`%c${label}: %c${content}`, colors.info, colors.title)
+}
 
 const writeProductionMessage = () => {
-  console.error("%cSorry, logs are deactivated. We are in production mode.", colors.title);
-};
+  console.error('%cSorry, logs are deactivated. We are in production mode.', colors.title)
+}
 
 class ConsoleLogAdvanced {
   constructor({ isDevelopMode }) {
-    this[_isDevelop] = isDevelopMode;
-    this[_count] = 0;
+    this[_isDevelop] = isDevelopMode
+    this[_count] = 0
   }
 
-  logger({ name, value, path, line, comment, date, time, isActive = true }) {
-    if (this._isDevelop) {
-      this.#_develop({ name, value, path, line, comment, date, time, isActive });
+  logger({ name, value, path, line, comment, date, time, collapsed = true, isActive = true }) {
+    if (this[_isDevelop]) {
+      this.#_develop({ name, value, path, line, comment, date, time, collapsed, isActive })
     } else {
-      this.#_production();
+      this.#_production()
     }
   }
 
-  #_develop({ name, value, path, line, comment, date, time, isActive }) {
-    if (!isActive) return;
+  #_develop({ name, value, path, line, comment, date, time, collapsed, isActive }) {
+    if (!isActive) return
 
-    logHeader();
-    logDetails("Path", path);
-    logDetails("Line", line);
-    logDetails("Date", date);
-    logDetails("Time", time);
-    logDetails("Type", getDataType(value));
-    logDetails("Name", name);
+    logHeader(name, collapsed)
+    logDetails('Path', path)
+    logDetails('Line', line)
+    logDetails('Date', date)
+    logDetails('Time', time)
+    logDetails('Type', getDataType(value))
+    logDetails('Name', name)
 
-    if (getDataType(value) === "Array" || getDataType(value) === "Object") {
-      console.table(value);
+    if (getDataType(value) === 'Array' || getDataType(value) === 'Object') {
+      console.table(value)
     } else {
-      console.info(value);
+      console.info(value)
     }
 
-    logDetails("Comment", comment);
-    logFooter();
+    logDetails('Comment', comment)
+    logFooter()
   }
 
   #_production() {
-    this._count++;
+    this._count++
     if (this._count === 1) {
-      writeProductionMessage();
+      writeProductionMessage()
     }
   }
 }
